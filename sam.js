a55f313b48039b393d8d9da8a583d9b472a19fd5
saivishnu725 <saivsihnu725@gmail.com>
2020-12-22 12:35:28 +0530

    Updated contact.html

---
+++ sam.js
@@ -0,0 +1,8 @@
+<script language="JavaScript" type="text/javascript">
+var link = "Mail me!";
+var subject = document.getElementById("subject").test.value;
+var body = document.getElementById("message").test.value;
+function protectmail2() {
+document.write('<a href=mailto:' + 'saivishnu725@gmail.com' + '?subject=' + subject + '&body=' + body + '>' + '</a>');
+}
+</script>
