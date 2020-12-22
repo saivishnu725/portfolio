df054c879decaefec25fe0307b677fc8ef25047d
saivishnu725 <saivsihnu725@gmail.com>
2020-12-22 12:37:17 +0530

    Removed sam.js

--- sam.js
+++
@@ -1,8 +0,0 @@
-<script language="JavaScript" type="text/javascript">
-var link = "Mail me!";
-var subject = document.getElementById("subject").test.value;
-var body = document.getElementById("message").test.value;
-function protectmail2() {
-document.write('<a href=mailto:' + 'saivishnu725@gmail.com' + '?subject=' + subject + '&body=' + body + '>' + '</a>');
-}
-</script>
